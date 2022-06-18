7d6c736817a6a1cc88c21dbcb8680e7e99c47c3f
sivanadler <sadler2007@gmail.com>
2022-06-18 12:11:33 -0400

    beginning initial setup for website

---
+++ src/sections/AboutPage.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div className="about">
+        <h1>Hello About </h1>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+    </div>
+  );
+}
+
+export default AboutPage;
