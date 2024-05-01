6611d1c9993fec731763f2fcad33467079c21608
sivanadler <sadler2007@gmail.com>
2024-05-01 10:04:28 -0400

    Updates

---
+++ static/js/856.47782612.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksivan_website_2022=self.webpackChunksivan_website_2022||[]).push([[856],{856:function(t,a,n){n.d(a,{c7Z:function(){return i}});var e=n(983);function i(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 010-5 2.5 2.5 0 010 5z"}}]})(t)}}}]);
+//# sourceMappingURL=856.47782612.chunk.js.map
